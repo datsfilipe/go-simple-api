4b70ab31ce14b6cc8b429f7bda22cf9b6f394cfb
datsfilipe <datsfilipe.foss@proton.me>
2023-09-25 11:12:08 -0300

    feat: create server with /api route with json response

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
-package gosimpleapi
+package main
 
 import (
-	"fmt"
+	"log"
+    "net/http"
+	"encoding/json"
+    "github.com/gorilla/mux"
 )
 
 func main()  {
-	fmt.Println("Hello World!")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	})
+
+	log.Println("API running!")
+	http.ListenAndServe(":4000", router)
 }
